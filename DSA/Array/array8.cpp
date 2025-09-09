e855e3c35b8661e5ea8bdc7f0a496e3797da2ee5
Samarthya Patil <samarthyapatil1982@gmail.com>
2025-09-09 14:49:18 +0000

    hi

--- DSA/Array/array8.cpp
+++ DSA/Array/array8.cpp
@@ -4,8 +4,21 @@ using namespace std;
     int findkey(){
         int i ;
         int arr[5]={12,23,34,45,56};
-    int k = 45;
+    int k = 99;
     for (i=0;i<5;i++)
-        if (arr[i]==k){return 1 };
+        if (arr[i]==k){return i ;}
+        return -1 ;
+}
 
-int arr[5]=(12,23,34,45,56);
+int main(){
+ int ans = findkey();
+  if (ans==-1){
+    cout<<"Element not found ";
+    
+  }
+  else{
+    cout<<"Element fount at "<<ans;
+
+  }
+}
+
--- DSA/Array/arraybinary.cpp
+++ DSA/Array/arraybinary.cpp
@@ -1,30 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int binarySerach(int a[], int beg ,int end,int mid ,int k )
-{
-int mid;
-    if (end > beg);
-    mid= (beg+end) /2;
-    {
-        if(a[mid]==k )    
-        {                 
-            return mid+1;    
-        } 
-        else if (a[mid]<k)
-        {  
-            return binarySerach(a,mid+1, end, k);    
+int binarySearch(int a[], int beg, int end, int k) {
+    if (beg < end) {
+        int mid = (beg + end) / 2;
+
+        if (a[mid] == k)
+            return mid;  
+        else if (a[mid] < k)
+            return binarySearch(a, mid+1, end, k); 
+        else
+            return binarySearch(a, beg,mid-1, k); 
         }
-        else     
-        {  
-            return binarySerach(a,mid-1,beg ,k);    
-        }          
-    }   
-    return -1;
+            return -1;
 }
 
-int main(){
-    int a[]={12,23,34,45,56,67,78,89};
-    int k=45;
+int main() {
+    int a[] = {12, 23, 34, 45, 56, 67, 78, 89};
+    int k = 45;
+    int n = sizeof(a) / sizeof(a[0]);
+
+    int ans = binarySearch(a, 0,n-1, k);
+    if (ans ==-1)
+        cout << "Element not found : " << endl;
+    else
+        cout << "Element found at index: " <<ans<<endl;
 
 }
