5ccfcaf40eec3dc13ab0d4e891fcab54d7ef058f
Samarthya Patil <samarthyapatil1982@gmail.com>
2025-09-16 15:21:20 +0000

    hi

--- DSA/Array/arraybinary.cpp
+++ DSA/Array/arraybinary.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
+
+
+int main() {
+    int a[] = {12, 23, 34, 45, 56, 67, 78, 89};
+    int k = 45;
+     
+
+    int ans = binarySearch(a, 0,8-1, k);
+    if (ans!=-1)
+          cout << "Element found at index: " <<ans<<endl;
+    else
+        cout << "Element not found " <<endl;
+
+}  
+
 int binarySearch(int a[], int beg, int end, int k) {
     if (beg < end) {
         int mid = (beg + end) / 2;
@@ -14,16 +29,3 @@ int binarySearch(int a[], int beg, int end, int k) {
         }
         return -1;
 }
-
-int main() {
-    int a[] = {12, 23, 34, 45, 56, 67, 78, 89};
-    int k = 45;
-    int n = sizeof(a) / sizeof(a[0]);
-
-    int ans = binarySearch(a, 0,n-1, k);
-    if (ans!=-1)
-          cout << "Element found at index: " <<ans<<endl;
-    else
-        cout << "Element not found " <<endl;
-
-}  
