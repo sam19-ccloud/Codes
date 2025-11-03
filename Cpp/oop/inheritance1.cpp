c5a5db0e50b018fd69b462b628b45e8e5accf435
Samarthya Patil <samarthyapatil1982@gmail.com>
2025-11-03 14:47:28 +0000

    hi

--- Cpp/oop/inheritance1.cpp
+++ Cpp/oop/inheritance1.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
 using namespace std;
+class Base{
+public:  
+    int x;  
+};  
+  
+class Derived:public Base{  
+public:  
+    int y;  
+};  
+  
+int main()  
+{ Derived Tpoint;    
+    Tpoint.y = 10;  
+    Tpoint.x = 15;  
+    cout<<"Derived Class Value: "<<Tpoint.y<<endl;  
+    cout<<"Base Class Value: "<<Tpoint.x<<endl;  
+  
+    return 0;  
+}  
